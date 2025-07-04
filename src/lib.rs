dacaf7d25c4cf31e4d83565030430805f804a905
Sam Willcocks <sam@wlcx.cc>
2025-07-04 18:46:14 +0100

    docs: improve module level doc

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,20 @@
 //! particularly on multiviewers/under monitor displays/broadcast monitors. It was originally
 //! used over serial, but is commonly also sent over IP. v5 explicitly supports this.
 //!
-//! An existing buffer of bytes is interpreted as a TSL UMD packet with the `new_checked` functions:
 //! ```rust
 //!   use tsl_umd::v3_1::{TSL31Packet, PACKET_LENGTH_31};
-//!   let raw = [0u8; PACKET_LENGTH_31];
-//!   let mut p = TSL31Packet::new_unchecked(raw);
+//!   // Build a new packet in a buffer:
+//!   let mut raw = [0u8; PACKET_LENGTH_31];
+//!   let mut p = TSL31Packet::new_unchecked(&mut raw);
 //!   p.set_address(13);
 //!   p.set_display_data("hello");
 //!   p.set_tally([true, false, false, false]);
-//!   assert_eq!(p.address(), 13);
-//!   assert_eq!(p.display_data(), "hello");
-//!   assert!(p.tally()[0]);
+//!
+//!   // Take a buffer and check that it's a valid packet, then access fields within it:
+//!   let packet = TSL31Packet::new_checked(raw).unwrap();
+//!   assert_eq!(packet.address(), 13);
+//!   assert_eq!(packet.display_data(), "hello");
+//!   assert!(packet.tally()[0]);
 //! ````
 #![no_std]
 pub mod v3_1;
